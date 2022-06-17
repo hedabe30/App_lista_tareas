a137bdf6823bcf3f91c491637ec03b93546fd7c3
hedabe30 <heyderbzr@gmail.com>
2022-06-17 12:13:40 -0500

    Se realiza el comienzo de lalogica del componente TaskSearch (clase 9)

--- src/components/TaskSearch.jsx
+++ src/components/TaskSearch.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/TaskSearch.css';
 
 const TaskSearch = () => {
+
+    const [searchValue, setSearchValue] = useState('');
+
     const onSearchValueChange = (event) => {
         let change = event.target.value;
-        console.log(change)
+        console.log(change);
+        setSearchValue(event.target.value);
     }
 
-    return (  
+    return (
+    <React.Fragment> 
         <input 
             className='TaskSearch'
-            type="text" 
             placeholder="Buscador" 
+            value={searchValue}
             onChange={onSearchValueChange}
         />
-    );
+        <p>{searchValue}</p>
+    </React.Fragment> 
+    )
 }
  
 export {TaskSearch};
